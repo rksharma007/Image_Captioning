ec0c6ae5f3084fd0adb54f1d5046fdb7944a59d1
Ravi Kant Sharma <77050199+rksharma007@users.noreply.github.com>
2022-04-24 19:43:39 +0530

    Add files via upload

---
+++ testing_caption_generator.py
@@ -0,0 +1,70 @@
+from pickle import load
+
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.applications.xception import Xception
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from PIL import Image
+
+img_path = 'C:/Users/theycallmeBOT/Desktop\ML/analytics_dataset/Flickr8k_Dataset/Flicker8k_Dataset/111537222_07e56d5a30.jpg'
+
+def extract_features(filename, model):
+        try:
+            image = Image.open(filename)
+            
+        except:
+            print("ERROR: Couldn't open image! Make sure the image path and extension is correct")
+        image = image.resize((299,299))
+        image = np.array(image)
+        # for images that has 4 channels, we convert them into 3 channels
+        if image.shape[2] == 4: 
+            image = image[..., :3]
+        image = np.expand_dims(image, axis=0)
+        image = image/127.5
+        image = image - 1.0
+        feature = model.predict(image)
+        return feature
+
+def word_for_id(integer, tokenizer):
+ for word, index in tokenizer.word_index.items():
+     if index == integer:
+         return word
+ return None
+
+
+def generate_desc(model, tokenizer, photo, max_length):
+    in_text = 'start'
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        pred = model.predict([photo,sequence], verbose=0)
+        pred = np.argmax(pred)
+        word = word_for_id(pred, tokenizer)
+        if word is None:
+            break
+        in_text += ' ' + word
+        if word == 'end':
+            break
+    return in_text
+
+
+#path = 'Flicker8k_Dataset/111537222_07e56d5a30.jpg'
+max_length = 32
+tokenizer = load(open("tokenizer.p","rb"))
+model = load_model('Models/model_9.h5')
+xception_model = Xception(include_top=False, pooling="avg")
+
+photo = extract_features(img_path, xception_model)
+img = Image.open(img_path)
+
+description = generate_desc(model, tokenizer, photo, max_length)
+print("\n")
+#print(description)
+result = description
+result = result.split(' ', 1)[1]
+result = result.rsplit(' ', 1)[0]
+print(result)
+plt.imshow(img)
+
